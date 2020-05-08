27c322fa6ec510ad996e8d0e0a32f0977c97565a
rgs1968 <rhonda@rhondasampson.com>
2020-05-08 03:07:50 -0400

    Add files via upload

---
+++ QuickDBD-ERD-5.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/AroeNX
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(255)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(255)   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" VARCHAR(255)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" VARCHAR(255)   NOT NULL,
+    "to_date" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "birth_date" VARCHAR(255)   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "gender" VARCHAR(255)   NOT NULL,
+    "hire_date" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" timestamp   NOT NULL,
+    "to_date" timestamp   NOT NULL
+);
+
+ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Dept_Manager" ("dept_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
